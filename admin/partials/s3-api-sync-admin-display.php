6560a49fc269ae72a67bb643cb080a4e5dbfc26c
Xavier <munroexavier@gmail.com>
2019-03-22 19:42:33 -0400

    added ability to toggle between saving all post and just s3 content.

--- admin/partials/s3-api-sync-admin-display.php
+++ admin/partials/s3-api-sync-admin-display.php
@@ -14,10 +14,19 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
 <form method="post" action="options.php">
     <?php settings_fields( 's3-api-sync-settings-group' ); ?>
     <?php do_settings_sections( 's3-api-sync-settings-group' ); ?>
     <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Only S3 Content</th>
+        <td><input type="checkbox" name="only-save-s3" value="true" <?php 
+                if (get_option('only-save-s3')==true) echo 'checked="checked" '; 
+            ?>/>
+        </td>
+        </tr>
+    
         <tr valign="top">
         <th scope="row">AWS Access Key ID</th>
         <td><input type="text" name="aws-access-key-id" value="<?php echo esc_attr( get_option('aws-access-key-id') ); ?>" /></td>
@@ -37,8 +46,17 @@
         <th scope="row">AWS Bucket</th>
         <td><input type="text" name="aws-bucket" value="<?php echo esc_attr( get_option('aws-bucket') ); ?>" /></td>
         </tr>
+
+        <tr valign="top">
+        <th scope="row">S3 Post Type</th>
+        <td><input type="text" name="s3-post-type" value="<?php 
+            echo esc_attr( get_option('s3-post-type')); 
+            ?>" 
+            <?php if (!get_option('only-save-s3')==true) echo 'disabled '; ?>
+            />
+        </td>
+        </tr>
     </table>
-    
     <?php submit_button(); ?>
 
 </form>
